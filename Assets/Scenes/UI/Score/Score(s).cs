937ad3eb9a82d6dc9c55953bc64fad26d2aa40ff
ren <KDT24GPR30003@edu.kobe-tech.ac.jp>
2025-06-28 18:24:31 +0900

    UI追加

---
+++ Assets/Scenes/UI/Score/Score(s).cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;  // 追加
+
+public class ScoreManager2 : MonoBehaviour
+{
+
+
+    public GameObject score_object = null; // Textオブジェクト
+    public int score_num = 0; // スコア変数
+
+    // 初期化
+    void Start()
+    {
+    }
+
+    // 更新
+    void Update()
+    {
+        // オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text>();
+        // テキストの表示を入れ替える
+        score_text.text = score_num + "s";
+
+        // test (スコア増加)
+        /*score_num += 1;*/ 
+
+    }
+}
---
+++ Assets/Scenes/UI/pause/Pause_Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;  // ’Ç‰Á
+
+public class Pause_Menu : MonoBehaviour
+{
+    // ‰Šú‰»
+    void Start()
+    {
+    }
+
+    // XV
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+        }
+
+    }
+}
